edf9f536f659d89ae0a6d973ac2cc209b676249d
Deependra Tripathi <dtripathi@tavisca.com>
2017-10-03 11:26:13 +0530

    crud operations in webapi

---
+++ WebApiDemo/ProductsData/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsData
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsData;
+namespace WebApiDemo.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        ProductDBEntities entity;
+        public ProductsController()
+        {
+            entity = new ProductDBEntities();
+        }
+        public IEnumerable<ProductsTable> Get()
+        {
+            //using (ProductDBEntities entities = new ProductDBEntities())
+            //{
+            //    return entities.ProductsTables.ToList();
+            //}
+            return entity.ProductsTables.ToList();
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            var en = entity.ProductsTables.FirstOrDefault(p => p.ID == id);
+            if (entity == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Product with the Id " + id.ToString() + "not found in the database");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+        public void Post([FromBody]ProductsTable product)
+        {
+            entity.ProductsTables.Add(product);
+            entity.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+           var en = entity.ProductsTables.FirstOrDefault(p => p.ID == id);
+            entity.ProductsTables.Remove(en);
+            entity.SaveChanges();
+        }
+       public void Put(int id,[FromBody]ProductsTable product)
+        {
+            var en = entity.ProductsTables.FirstOrDefault(p => p.ID == id);
+            en.Name = product.Name;
+            en.Price = product.Price;
+            en.Quantity = product.Quantity;
+            entity.SaveChanges();
+        }
+    }
+}
